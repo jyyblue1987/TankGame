ecc431181a3d825eb4ec477712206a890813af4e
jyyblue <jyyblue1987@outlook.com>
2021-04-28 12:20:09 +0800

    [create] create project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# CSC 413 Term Project README
+
+Name:Rai'd Muhammad 
+
+Email:rmuhammad@mail.sfsu.edu
+
+Extra Features:
---
+++ src/java/edu/csc413/tankgame/model/GameWorld.java
@@ -0,0 +1,53 @@
+package edu.csc413.tankgame.model;
+
+import edu.csc413.tankgame.Constants;
+
+import java.util.*;
+
+/**
+ * GameWorld holds all of the model objects present in the game. GameWorld tracks all moving entities like tanks and
+ * shells, and provides access to this information for any code that needs it (such as GameDriver or entity classes).
+ */
+public class GameWorld {
+    // TODO: Implement. There's a lot of information the GameState will need to store to provide contextual information.
+    //       Add whatever instance variables, constructors, and methods are needed.
+
+    List<Entity> entities = new ArrayList<Entity>();
+
+    public GameWorld() {
+    }
+
+    /** Returns a list of all entities in the game. */
+    public List<Entity> getEntities() {
+        // TODO: Implement.
+        return entities;
+    }
+
+    /** Adds a new entity to the game. */
+    public void addEntity(Tank entity) {
+        entities.add(entity);
+    }
+
+    /** Returns the Entity with the specified ID. */
+    public Entity getEntity(String id) {
+        // TODO: Implement.
+        for(Entity entity : entities)
+        {
+            Tank tank = (Tank) entity;
+            if(tank.getId().equals(id))
+                return entity;
+        }
+        return null;
+    }
+
+    /** Removes the entity with the specified ID from the game. */
+    public void removeEntity(String id) {
+        // TODO: Implement.
+        for(Entity entity : entities)
+        {
+            Tank tank = (Tank) entity;
+            if(tank.getId().equals(id))
+                entities.remove(entity);
+        }
+    }
+}
